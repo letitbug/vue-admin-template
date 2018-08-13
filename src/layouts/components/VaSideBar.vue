a9cec2367bbccc272094976a8e42362728cd2083
letsbug <xman724939995@gmail.com>
2018-08-13 18:08:01 +0800

    Add sidebar brand stylesheet.

--- src/layouts/components/VaSideBar.vue
+++ src/layouts/components/VaSideBar.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="va-sidebar"></div>
+  <div class="va-sidebar">
+    <div class="va-side-brand">
+      <img class="brand-logo" :src="logo" alt="">
+      <div class="brand-name">VA MINI</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'VaSideBar'
+  name: 'VaSideBar',
+  data() {
+    return {
+      logo: require('@/assets/logo.png')
+    }
+  }
 }
 </script>
