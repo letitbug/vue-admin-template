b3b303c978693d00f96dcba55b99446668fbb56e
letsbug <xman724939995@gmail.com>
2018-08-13 16:06:30 +0800

    Add the logical code for device adaptation and transformation.

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -13,24 +13,48 @@
 
 <script>
 import { VaSideBar, VaHeadBar, VaTabsBar, VaFootBar,Breadcrumb,  AppBody } from './components'
-import SidebarResizeHandler from '@/mixins/SidebarResizeHandler'
+
+const { body } = document
+const WIDTH = 768
 
 export default {
   name: 'Layout',
   components: { VaSideBar, VaHeadBar, VaTabsBar, VaFootBar, Breadcrumb, AppBody },
-  mixins: [SidebarResizeHandler],
+  watch: {
+    $route() {
+      // In mobile devices, auto close the sidebar when route jump.
+      if (this.isMobile() && this.sidebarOpened) {
+        this.$store.dispatch('app_sidebar_close')
+      }
+    }
+  },
+  beforeMount() {
+    window.addEventListener('resize', this.resizeHandler)
+  },
+  mounted() {
+    const isMobile = this.isMobile()
+    if (isMobile) {
+      this.$store.dispatch('app_device_toggle', 'mobile')
+    }
+  },
   computed: {
-    sidebarOpened() {
-      const opened = this.$store.state.application.sidebar.opened
-      console.log(opened)
-      return opened
-    },
+    sidebarOpened() { return this.$store.state.application.sidebar.opened },
     sidebarStatus() { return 'sidebar-' + (this.sidebarOpened ? 'expanded' : 'collapse') },
     device() { return this.$store.state.application.device }
   },
   methods: {
     toggleDevice() { this.device = this.device === 'desktop' ? 'mobile' : 'desktop' },
-    closeSidebar() { this.$store.dispatch('app_sidebar_close') }
+    closeSidebar() { this.$store.dispatch('app_sidebar_close') },
+    isMobile:() => body.getBoundingClientRect().width <= WIDTH,
+    resizeHandler() {
+      const isMobile = this.isMobile()
+      this.$store.dispatch('app_device_toggle', isMobile ? 'mobile' : 'desktop')
+
+      if (isMobile) {
+        this.$store.dispatch('app_sidebar_close')
+      }
+      // else store.dispatch('app_sidebar_open')
+    }
   }
 }
 </script>
