8810bec49f8147f4b716bcdee0e5a2c90aac7865
letsbug <xman724939995@gmail.com>
2018-08-16 18:01:42 +0800

    Delete all stylesheets in the side-menu.

--- src/components/sidebar-menus/VaSideMenu.vue
+++ src/components/sidebar-menus/VaSideMenu.vue
@@ -2,34 +2,37 @@
 <div class="va-side-main">
 
   <!-- About main functions -->
-  <div class="va-side-menus" ref="menu1">
-    <h6 class="va-side-title expander" @click.stop.prevent="expandMenu(1)">Main <i
-      class="el-icon-arrow-right expand-handler"></i></h6>
+  <div class="va-side-nav" ref="menu1">
+    <a class="va-side-nav-title expander" @click.stop.prevent="expandMenu(1)">
+      <svg class="va-icon" aria-hidden="true"><use xlink:href="#if-home"></use></svg><span class="item-name">main</span>
+      <i class="el-icon-arrow-right expander-icon"></i>
+    </a>
     <ul class="va-side-menu">
       <li><router-link to="/dashboard" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-dashboard"></use></svg> <span class="item-name">Dashboard</span></router-link></li>
+        xlink:href="#if-dashboard"></use></svg> <span class="item-name">dashboard</span></router-link></li>
       <li><router-link to="/report" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-chart"></use></svg> <span class="item-name">Reports</span></router-link></li>
+        xlink:href="#if-chart"></use></svg> <span class="item-name">reports</span></router-link></li>
       <li><router-link to="/table" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-table"></use></svg> <span class="item-name">Tables</span></router-link></li>
+        xlink:href="#if-table"></use></svg> <span class="item-name">tables</span></router-link></li>
       <li><router-link to="/others" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-apps"></use></svg> <span class="item-name">Others</span></router-link></li>
+        xlink:href="#if-apps"></use></svg> <span class="item-name">others</span></router-link></li>
     </ul>
   </div>
 
   <!-- About user guide -->
-  <div class="va-side-menus" ref="menu2">
-    <h6 class="va-side-title expander" @click.stop.prevent="expandMenu(2)">Settings <i
-      class="el-icon-arrow-right expand-handler"></i></h6>
+  <div class="va-side-nav" ref="menu2">
+    <a class="va-side-nav-title expander" @click.stop.prevent="expandMenu(2)">
+      <svg class="va-icon" aria-hidden="true"><use xlink:href="#if-settings"></use></svg><span class="item-name">settings</span>
+      <i class="el-icon-arrow-right expander-icon"></i></a>
     <ul class="va-side-menu">
       <li><router-link to="/error/500" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-handle-browse"></use></svg> <span class="item-name">Website</span></router-link></li>
+        xlink:href="#if-handle-browse"></use></svg> <span class="item-name">website</span></router-link></li>
       <li><router-link to="/error/403" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-user"></use></svg> <span class="item-name">Account</span></router-link></li>
+        xlink:href="#if-user"></use></svg> <span class="item-name">account</span></router-link></li>
       <li><router-link to="/error/402" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-handle-browse"></use></svg> <span class="item-name">Rules</span></router-link></li>
+        xlink:href="#if-handle-browse"></use></svg> <span class="item-name">rules</span></router-link></li>
       <li><router-link to="/error/401" class="menu-item"><svg class="va-icon" aria-hidden="true"><use
-        xlink:href="#if-settings"></use></svg> <span class="item-name">Client</span></router-link></li>
+        xlink:href="#if-settings"></use></svg> <span class="item-name">client</span></router-link></li>
     </ul>
   </div>
 </div>
