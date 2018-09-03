59e35e5dffd12d2dd968c8399b1153dfc61d6fe4
letsbug <xman724939995@gmail.com>
2018-09-03 11:06:33 +0800

    update doc files.

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -84,7 +84,7 @@ module.exports = new Promise((resolve, reject) => {
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
-          messages: [`Your application 'vue-admin-template' is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+          messages: [`Your application 'vava-mini' is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
         ? utils.createNotifierCallback()
