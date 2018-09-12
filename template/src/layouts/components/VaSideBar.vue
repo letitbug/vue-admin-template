95ce527a5f3c1f9a5ffb0ef8938b4b152dd5bcf8
letsbug <xman724939995@gmail.com>
2018-09-12 23:25:42 +0800

    Modified sidebar bg color.

--- template/src/layouts/components/VaSideBar.vue
+++ template/src/layouts/components/VaSideBar.vue
@@ -2,7 +2,7 @@
   <div class="va-sidebar">
     <div class="va-side-brand">
       <img class="brand-logo" :src="logo" alt="">
-      <span class="brand-name">vava mini</span>
+      <span class="brand-name">Vava mini</span>
     </div>
     <va-side-menu></va-side-menu>
   </div>
