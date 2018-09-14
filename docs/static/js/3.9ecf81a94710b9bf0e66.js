314c982c3d01fd4427cd632baa3a656d9de5c5c2
letsbug <xman724939995@gmail.com>
2018-09-14 18:11:24 +0800

    Modify the package directory location, Update docs.

---
+++ docs/static/js/3.9ecf81a94710b9bf0e66.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{Cka3:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"va-body-container no-bg"},[t("h1",[this._v("Dashboard")])])}]},a=n("VU/8")({name:"Dashboard"},s,!1,null,null,null);t.default=a.exports}});
+//# sourceMappingURL=3.9ecf81a94710b9bf0e66.js.map
