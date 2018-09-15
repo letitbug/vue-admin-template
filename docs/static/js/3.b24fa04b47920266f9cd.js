189d96a05c2122e3b4854d79b7f9f91a61f6ed10
letsbug <xman724939995@gmail.com>
2018-09-15 16:40:49 +0800

    Update doc.

--- docs/static/js/3.b24fa04b47920266f9cd.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{Cka3:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"va-body-container no-bg"},[t("h1",[this._v("Dashboard")])])}]},a=n("VU/8")({name:"Dashboard"},s,!1,null,null,null);t.default=a.exports}});
-//# sourceMappingURL=3.b24fa04b47920266f9cd.js.map
